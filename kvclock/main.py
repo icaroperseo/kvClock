9b15a707ebcd4deb67f5eef71892f5f0c9655c04
Icaro Perseo <icaroperseo@protonmail.com>
2017-04-15 00:43:44 -0500

    Finalized UI

--- kvclock/main.py
+++ kvclock/main.py
@@ -5,20 +5,26 @@ import kivy
 kivy.require('1.9.1')
 
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.utils import get_color_from_hex
 
 __author__ = "Icaro Perseo"
 __copyright__ = "Icaro Perseo"
 __license__ = "gpl3"
 
 
-class Clock(Widget):
+class ClockUI(BoxLayout):
     pass
 
 
 class ClockApp(App):
     def build(self):
-        return Clock()
+        return ClockUI()
 
 if __name__ == '__main__':
+    Window.clearcolor = get_color_from_hex('#101216')
+    LabelBase.register(name='Roboto', fn_regular='assets/fonts/Roboto-Thin.ttf',
+                       fn_bold='assets/fonts/Roboto-Medium.ttf')
     ClockApp().run()
